dd8c919732afd0cb22e2464151c3b05086163d33
Alex Evans <aevans@quiktrip.com>
2024-01-30 14:50:02 -0600

    added event listeners to lightbulbs

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Write your code here
+const lightBulb1 = document.querySelector("#lightbulb1")
+const lightBulb2 = document.querySelector("#lightbulb2")
+const lightBulb3 = document.querySelector("#lightbulb3")
+let subtitle = document.querySelector('.subtitle')
+let count = 0
+
+lightBulb1.addEventListener("click", function(){
+    count++
+    lightBulb1.classList.toggle('active')
+    subtitle.innerHTML = `You've clicked the lightbulbs ${count} times`
+})
+
+lightBulb2.addEventListener("click", function(){
+    count++
+    lightBulb2.classList.toggle('active')
+    subtitle.innerHTML = `You've clicked the lightbulbs ${count} times`
+})
+
+lightBulb3.addEventListener("click", function(){
+    count++
+    subtitle.innerHTML = `You've clicked the lightbulbs ${count} times`
+    lightBulb3.classList.toggle('active')
+    
+})
+
